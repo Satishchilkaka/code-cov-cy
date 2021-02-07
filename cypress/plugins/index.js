4ebaa21406a1b057b8a54865d853a0d8a5d26416
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-02-06 22:16:27 -0500

    added code coverage for angular webapp

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -23,3 +23,37 @@ module.exports = (on, config) => {
   // with any changed environment variables
   return config
 }
+
+/* // ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// const wp = require('./cy-ts-preprocessor')
+ /*
+const admin = require("firebase-admin");
+const cypressFirebasePlugin = require("cypress-firebase").plugin;
+
+module.exports = (on, config) => {
+
+  const extendedConfig = cypressFirebasePlugin(on, config, admin);
+  // include any other plugin code...
+  on('file:preprocessor', cypressTypeScriptPreprocessor);
+
+  require('@cypress/code-coverage/task')(on, config)
+
+  // It's IMPORTANT to return the config object
+  // with any changed environment variables
+  return { extendedConfig, config }
+} */
